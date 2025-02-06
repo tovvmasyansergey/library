e8285bced41b0bf39e8969c10f17d1ef5b7ba705
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2025-02-06 16:12:58 +0400

    js,html,css examples

---
+++ src/main/java/com/example/library/frontend/dom/app1.js
@@ -0,0 +1,28 @@
+// const div = document.createElement('div')
+// div.classList.add('wrapper')
+//
+// const body = document.body
+// body.append(div)
+//
+// const header = document.createElement('h1')
+// header.textContent = 'DOM Hello'
+//
+// div.insertAdjacentElement('beforebegin',header)
+//
+// const ul = `
+//     <ul>
+//         <li>1</li>
+//         <li>2</li>
+//         <li>3</li>
+//     </ul>`
+// div.innerHTML = ul
+//
+// const img = document.createElement('img')
+// img.src = "image/a12854444.jpg"
+// img.width = 240
+// div.appendChild(img)
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     let h = document.querySelector('h1');
+//     document.write(h.textContent);
+// });
